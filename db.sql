9bbdeaa5b4050ceddcbb0fb3827af977dc30e7e7
EunChan Park <pec9399@naver.com>
2018-05-28 10:42:30 +0900

    [Add] Database and Table Setup

---
+++ db.sql
@@ -0,0 +1,21 @@
+CREATE DATABASE DevTools;
+USE DevTools;
+
+CREATE TABLE User(
+	uid VARCHAR(20) NOT NULL,
+	pwd VARCHAR(64) NOT NULL,
+	sex INTEGER(1),
+	PRIMARY KEY (uid)
+);
+
+CREATE TABLE Dev(
+	id INTEGER AUTO_INCREMENT NOT NULL,
+	name TEXT,
+	version TEXT,
+	insPath TEXT,
+	details TEXT,
+	reference TEXT,
+	uid VARCHAR(20),
+	PRIMARY KEY (id),
+	FOREIGN KEY (uid) REFERENCES User(uid)
+);
