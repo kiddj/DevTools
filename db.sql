df3e1faeaaeb52e9d703a53d725b6b58d40b8e44
EunChan Park <pec9399@naver.com>
2018-06-05 22:22:41 +0900

    [Edit] DB Schema

--- db.sql
+++ db.sql
@@ -18,14 +18,14 @@ CREATE TABLE Dev(
 	details TEXT,
 	reference TEXT,
 	uid VARCHAR(20),
-	template TEXT,
+	template VARCHAR(30) NOT NULL,
 	PRIMARY KEY (id),
 	FOREIGN KEY (uid) REFERENCES User(uid),
 	FOREIGN KEY (template) REFERENCES Template(name)
 );
 
 CREATE TABLE Template(
-	name TEXT NOT NULL,
+	name VARCHAR(30) NOT NULL,
 	createdBy VARCHAR(20),
 	PRIMARY KEY (name),
 	FOREIGN KEY (createdBy) REFERENCES User(uid)
